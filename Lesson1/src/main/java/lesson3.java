3aaabbc9747aa20bd23acc09ef60e9e4a9a160c2
iBekas <dmitrbekasov@gmail.com>
2021-04-30 01:34:55 +0300

    Практическое задание 3

--- Lesson1/src/main/java/lesson3.java
+++ Lesson1/src/main/java/lesson3.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Random;
 import java.util.Scanner;
 
 public class lesson3 {
@@ -8,36 +11,68 @@ public class lesson3 {
 
         // Задание 3 - мещение эллементов массива на n, метод -arrShift;
         int[] arrForShift = {1, 2, 3, 4, 5};
-        int shift = 2;
+        int shift = 1;
         System.out.print("Задание 3 - ");
         arrShift(arrForShift, shift);
+        System.out.println(" ");
+
+        // Задание 2 выполнил в прошлом ДЗ
 
         // Задание 1 - игра, метод - game
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-
         game(words);
+
     }
 
     private static void arrShift(int[] arr, int shift) {
+        int temp;
         for (int i = 0; i < arr.length; i++) {
             int x = arr.length - (shift - i);
             while (true) {
-                if(x >= arr.length && x>0) {
+                if(x >= arr.length) {
                     x-=5;
                 }else if (x<0) {
                     x+=5;
-                }else if (x>=0 && x< arr.length) {
-                    break;
-                }
+                }else break;
             }
+            temp = arr[i];
             arr[i] = arr[x];
+            System.out.print(arr[x] + " ");
 
         }
     }
 
 
     private  static void game (String [] arr) {
-
+        while (true) {
+            char a;
+            char b;
+            Random random = new Random();
+            int answer = random.nextInt(arr.length);
+            System.out.print("Я загадал одно из этих слов - " + Arrays.toString(arr) + "\nКак ты думаешь какое слово я загадал? >>> ");
+            String userAnswer = scanner.next();
+            userAnswer = userAnswer.toLowerCase(Locale.ROOT);
+            if (arr[answer].equals(userAnswer)) {
+                System.out.println("Ты угадал!!!");
+            } else {
+                System.out.println("Ты не угадал. Попробуй еще раз, держи подсказку! Если буквы совпали, они появятся всмето #.");
+                for (int i = 0; i < 15; i++) {
+                    String test1 = arr[answer] + "................."; // я знаю, что это ОЧЕНЬ изящное решение, но уже 12 ночи, сделайте поблажку=)
+                    String test2 = userAnswer + ",,,,,,,,,,,,,,,,,";
+                    a = test1.charAt(i);
+                    b= test2.charAt(i);
+                    if (a==b) System.out.print(a);
+                    else System.out.print("#");
+                }
+                System.out.println(" ");
+                userAnswer = scanner.next();
+            }
+            System.out.println("Еще разок? 1- да, 0 - нет.");
+            int userChoice = scanner.nextInt();
+            if (userChoice != 1) break;
+        }
     }
+
+
 
 }
