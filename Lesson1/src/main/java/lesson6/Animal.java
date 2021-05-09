a5841eaad8f421a1988c3e017800a3766995cacb
iBekas <dmitrbekasov@gmail.com>
2021-05-10 01:01:08 +0300

    Задание 6 - финал

--- Lesson1/src/main/java/lesson6/Animal.java
+++ Lesson1/src/main/java/lesson6/Animal.java
@@ -1,4 +1,31 @@
 package lesson6;
 
-public class Animal {
+public abstract class Animal {
+
+    static int amountAnimal = 0;
+
+    protected String name;
+    protected String type;
+
+    public Animal(String type) {
+        this.type = type;
+    }
+
+    public void run (int distance) {
+        System.out.printf("%s %s пробежал %d метров.\n", this.type, this.name, distance);
+    }
+
+    public void swim (int distance) {
+        System.out.printf("%s %s проплыл %d метров.\n", this.type, this.name, distance);
+    }
+
+    static int amountAnimal() {return amountAnimal;}
+
+    static int amountAnimal2(Animal[] arr) {
+        int counter = 0;
+        for (Animal i: arr) {
+            if (i instanceof Animal) counter++;
+        }
+        return counter;
+    }
 }
--- Lesson1/src/main/java/lesson6/Dog.java
+++ Lesson1/src/main/java/lesson6/Dog.java
@@ -1,4 +1,39 @@
 package lesson6;
 
-public class Dog {
+public class Dog extends Animal{
+
+    static int amountDog = 0;
+
+    public Dog(String name) {
+        super("Пёс");
+        this.name = name;
+        amountDog++;
+        amountAnimal++;
+    }
+
+    @Override
+    public void run(int distance) {
+        if (distance > 500) System.out.printf("%s %s пробежал %d метров и умер.\n", this.type, this.name, distance);
+        else if (distance < 0) System.out.printf("%s %s не умеет бегать задом на перед.\n", this.type, this.name);
+        else super.run(distance);
+    }
+
+    @Override
+    public void swim(int distance) {
+        int maxDistance = 10;
+        if (distance > maxDistance) System.out.printf("%s %s проплыл %d метров и пошел ко дну.\n", this.type, this.name, maxDistance);
+        else if (distance < 0) System.out.printf("%s %s не понимает что ему делать.\n", this.type, this.name);
+        else super.swim(distance);
+    }
+
+    static int amountDog() {return amountDog;}
+
+    public static int amountDog2(Animal[] arr) {
+        int amountDog2 = 0;
+        for (Animal i: arr) {
+            if (i instanceof Cat) amountDog2++;
+        }
+        return amountDog2;
+    }
+
 }
