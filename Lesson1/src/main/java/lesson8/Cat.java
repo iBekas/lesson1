14478e1a0f7102da40f7de6849ab11dfd4bcfcc8
iBekas <dmitrbekasov@gmail.com>
2021-05-15 17:40:10 +0300

    Задание 8- start

---
+++ Lesson1/src/main/java/lesson8/Cat.java
@@ -0,0 +1,13 @@
+package lesson8;
+
+public class Cat implements Jumping, Running{
+    @Override
+    public void jump() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ Lesson1/src/main/java/lesson8/Robot.java
@@ -0,0 +1,13 @@
+package lesson8;
+
+public class Robot implements Jumping, Running{
+    @Override
+    public void jump() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
