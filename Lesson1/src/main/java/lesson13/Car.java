5dce9013daf45c9810cf1a4ec82e82a32a6c3c18
iBekas <dmitrbekasov@gmail.com>
2021-06-03 23:18:07 +0300

    Задание 13 - finish

--- Lesson1/src/main/java/lesson13/Car.java
+++ Lesson1/src/main/java/lesson13/Car.java
@@ -1,10 +1,17 @@
 package lesson13;
 
+
+import java.util.concurrent.BrokenBarrierException;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     private Race race;
     private int speed;
     private String name;
+    private long timeStart;
+    private long timeFinish;
+
+
 
     public String getName() {
         return name;
@@ -27,11 +34,26 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            RaceThreads.readyCheck.await();
+            Thread.sleep(100); // если это еще тут, значит я не понял, почему один из потоков постоянно обгоняетя "ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!"
+            this.timeStart = System.currentTimeMillis();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        this.timeFinish = System.currentTimeMillis() - timeStart; //несколько раз проверить, ошибок не нашел. У кого первого вывело в консоль "закончил этап: Дорога 40 метров" тот и победитель.
+        try {
+            RaceThreads.finishCheck.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    public long getTimeFinish() {
+        return timeFinish;
+    }
+
+
 }
--- Lesson1/src/main/java/lesson13/Tunnel.java
+++ Lesson1/src/main/java/lesson13/Tunnel.java
@@ -10,13 +10,16 @@ public class Tunnel extends Stage {
     public void go(Car c) {
         try {
             try {
+                RaceThreads.tunnel.acquire();
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+//                RaceThreads.tunnel.acquire(); кажется, что захват должен быть тут, это логичнее. Но тогда, тот кто первый или второй прошел первую дорогу, не всегда успевали попасть в тунель, переодически их кто-то обгонял, что мне не понятно, почему так?
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                RaceThreads.tunnel.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
